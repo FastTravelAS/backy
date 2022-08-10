addacbe88ebbf6fdde663887dc80e03a305a1d31
Alexey Kharchenko <akharchenko@gmail.com>
2022-08-10 16:09:41 +0600

    Merge pull request #1218 from rubynor/390-regular-db-backup-to-s3-2
    Create target directory while downloading the database dump

--- app/services/db/backup/s3_load.rb
+++ app/services/db/backup/s3_load.rb
@@ -18,6 +18,7 @@ module DB
 
           begin
             s3.get_object(response_target:, key:, bucket:)
+            FileUtils.mkdir_p(File.dirname(file_name))
             File.rename(response_target, file_name)
           rescue Aws::S3::Errors::NoSuchKey
             puts "error. No such key #{key}"
